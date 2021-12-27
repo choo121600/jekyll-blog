1a583dd1a148acffc85270c2dcc7f328bc0ae3ed
Keith Choo <choo121600@gmail.com>
2021-12-28 00:47:34 +0900

    Home renewal(Img Slider)

---
+++ js/slider.js
@@ -0,0 +1,33 @@
+const slides = document.querySelectorAll('.slide');
+const controls = document.querySelectorAll('.control');
+let activeSlide = 0;
+let prevActive = 0;
+
+changeSlides();
+let int = setInterval(changeSlides, 2000);
+
+function changeSlides() {
+	slides[prevActive].classList.remove('active');
+	controls[prevActive].classList.remove('active');
+
+	slides[activeSlide].classList.add('active');
+	controls[activeSlide].classList.add('active');
+	
+	prevActive = activeSlide++;
+	
+	if(activeSlide >= slides.length) {
+		activeSlide = 0;
+	}
+}
+
+controls.forEach(control => {
+	control.addEventListener('click', () => {
+		let idx = [...controls].findIndex(c => c === control);
+		activeSlide = idx;
+
+		changeSlides();
+
+		clearInterval(int);
+		int = setInterval(changeSlides, 4000);
+	});
+});
